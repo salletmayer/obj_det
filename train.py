e97b78cf13b17ee0ab239ba1a2e1c7814feb54b4
Sebastian Salletmayer <salletmayer.sebastian@gmail.com>
2024-01-18 19:31:54 +0000

    more training

--- train.py
+++ train.py
@@ -1,3 +1,27 @@
-import jason
+import torch
+from torch.utils.data import DataLoader
+from torchvision.models.detection.faster_rcnn import FasterRCNN
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.transforms import functional as F
+from PIL import Image
 
-def load_labels():
+# Define your custom dataset class
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, image_paths, targets):
+        self.image_paths = image_paths
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        image = Image.open(image_path).convert("RGB")
+        target = self.targets[idx]
+
+        return F.to_tensor(image), target
+
+# Define your custom DataLoader
+batch_size = 4
+custom_dataset = CustomDataset(image_paths, targets)
+data_loader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
